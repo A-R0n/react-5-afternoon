2aff0f7bc883db530657fa1c3e664cc092bc5f32
Aaron Estes <aestesc@gmail.com>
2018-11-15 14:19:27 -0600

    added state to App.js and wrapped everything in the Provider

--- src/components/WizardTwo/WizardTwo.js
+++ src/components/WizardTwo/WizardTwo.js
@@ -1,15 +1,18 @@
 import React,  { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { updateCity } from '../../ducks/reducer';
 
 class WizardTwo extends Component {
     render(){
+        const { updateCity } = this.props
         return(
             <div className="parent-div">
                 <div className="vert-align">
             
                 <p>In what city will the property be located?</p><br />
                     
-                        <input placeholder="city name" type="text" onChange={this.props.updateCity}/>
+                        <input placeholder="city name" type="text" onChange={(e) => updateCity(e.target.value)}/>
                 
                     <Link to="/wThree"><button className="wTwo-btn"> Next </button></Link>
                 </div>
@@ -17,5 +20,15 @@ class WizardTwo extends Component {
         )
     }
 }
-
-export default WizardTwo;
+//we are connecting the function onto the props object so that we can invoke it as an event handler
+// write exactly what you are bringing from state instead of returning all of state.
+// i fyou are not expliit you don't know if you are getting the value from the reducer or a parent component
+// writing is as following versus (state) => state.
+function mapStateToProps(state) {
+    const {city} = state;
+    return {
+        city
+    };
+}
+// dispatch to reducer, this is the middleman. This invokes the reducer
+export default connect(mapStateToProps, {updateCity})(WizardTwo);
